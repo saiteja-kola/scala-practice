273e4c6bc8f57c266a93daa0d18b97450ff53991
saiteja-kola <saiteja.kola99@gmail.com>
2017-07-20 15:03:37 -0400

    Initial commit

---
+++ src/main/scala/com/scala/practice/BMI.scala
@@ -0,0 +1,28 @@
+package com.scala.practice
+
+/**
+  * Created by skola on 7/20/2017.
+  */
+object BMI {
+
+
+  def main(args: Array[String]): Unit = {
+
+    println("Please Enter Your Weight: ")
+
+    val lbs = scala.io.StdIn.readDouble()
+    println("Please Enter Your Height In Inches: ")
+
+    val height = scala.io.StdIn.readDouble()
+
+
+    val bmi = lbs/(height*height) * 703
+
+    if(bmi<18.5)
+      println("Under Weight")
+    else if(bmi< 25)
+
+      println("Normal Weight")
+    else println("Over Weight")
+  }
+}
