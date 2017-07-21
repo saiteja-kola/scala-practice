a409427a7ba4e44ad515d6dbcf9aaa1bee5aca84
saiteja-kola <saiteja.kola99@gmail.com>
2017-07-21 11:20:14 -0400

    Scala Programs

--- src/main/scala/com/scala/calculator/Add.scala
+++ src/main/scala/com/scala/calculator/Add.scala
@@ -6,6 +6,10 @@ package com.scala.calculator
 object Add {
   def main(args: Array[String]): Unit = {
 
+
+
+
+
   }
 
 }
---
+++ src/main/scala/com/scala/collections/Hash.scala
@@ -0,0 +1,22 @@
+package com.scala.collections
+
+
+/**
+  * Created by skola on 7/20/2017.
+  */
+object Hash {
+
+  def main(args: Array[String]): Unit = {
+
+    var movietime :Map[String,String] = Map()
+
+    movietime = Map("Movie" -> "12,13,14", "Movie1" -> "14,15,16")
+
+
+
+
+
+
+  }
+
+}
